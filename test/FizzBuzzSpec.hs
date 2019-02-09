61c39a2b1ad27641ff4cf34270d84f73b180bc93
Masaya Nakamura <mashabow@shiromoji.net>
2019-02-09 12:12:11 +0900

    Add FizzBuzzSpec.hs with stub test cases for `convert`

---
+++ test/FizzBuzzSpec.hs
@@ -0,0 +1,19 @@
+module FizzBuzzSpec (spec) where
+
+import Test.Hspec
+import FizzBuzz
+
+spec :: Spec
+spec = do
+    describe "convert" $ do
+        context "3 の倍数のとき" $ do
+            it "Fizz を返す" $ do pending
+        context "5 の倍数のとき" $ do
+            it "Buzz を返す" $ do pending
+        context "3 と 5 の倍数のとき" $ do
+            it "FizzBuzz を返す" $ do pending
+        context "それ以外のとき" $ do
+            it "その自然数を文字列にして返す" $ do
+                convert 1 `shouldBe` "1"
+                convert 2 `shouldBe` "2"
+                convert 34 `shouldBe` "34"
