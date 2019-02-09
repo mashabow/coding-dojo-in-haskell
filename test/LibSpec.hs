2e4665dae9aa6a0e161975d4b0fef53079793672
Masaya Nakamura <mashabow@shiromoji.net>
2019-02-09 15:04:31 +0900

    Test stdout with `silently`
    📖 https://hackage.haskell.org/package/silently-1.2.5/docs/System-IO-Silently.html

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -1,10 +1,11 @@
 module LibSpec (spec) where
 
 import Test.Hspec
+import System.IO.Silently
 import Lib
 
 spec :: Spec
 spec = do
     describe "someFunc" $ do
-        it "ユニットを返す" $ do
-            someFunc `shouldReturn` ()
+        it "\"someFunc\\n\" という文字列を出力する" $ do
+            capture_ someFunc `shouldReturn` "someFunc\n"
