426e76b4920c8012cb2c1b87ace0b925531bd45a
Masaya Nakamura <mashabow@shiromoji.net>
2019-02-19 21:43:18 +0900

    Implement `splitIntoFrames`

--- src/Bowling.hs
+++ src/Bowling.hs
@@ -1,13 +1,35 @@
 module Bowling where
 
+import Data.Char
+
 
 -- http://codingdojo.org/kata/Bowling/
 
 
 data Frame =
-    OpenFrame Int Int |
-    SpareFrame Int |
     StrikeFrame |
+    SpareFrame Int |
+    OpenFrame Int Int |
     -- 第 10 フレームは内容によらず、すべて LastFrame で表す
     LastFrame Int Int Int
     deriving (Show)
+
+toInt :: Char -> Int
+toInt c = if c == '-'
+    then 0
+    else digitToInt c
+
+toNonLastFrame :: String -> Frame
+toNonLastFrame ['X'] = StrikeFrame
+toNonLastFrame [c1, '/'] = SpareFrame (toInt c1)
+toNonLastFrame [c1, c2] = OpenFrame (toInt c1) (toInt c2)
+
+toLastFrame :: String -> Frame
+toLastFrame [c1, c2] = LastFrame (toInt c1) (toInt c2) 0
+toLastFrame [c1, c2, c3] = LastFrame (toInt c1) (toInt c2) (toInt c3)
+
+splitIntoFrames :: String -> [Frame]
+splitIntoFrames s =
+    reverse (toLastFrame xLast : map toNonLastFrame xsReversed)
+    where
+        xLast:xsReversed = reverse . words $ s
---
+++ test/BowlingSpec.hs
@@ -0,0 +1,59 @@
+module BowlingSpec (spec) where
+
+import Test.Hspec
+import Bowling
+
+{-# ANN module "HLint: ignore Redundant do" #-}
+
+spec :: Spec
+spec = do
+    describe "splitIntoFrames" $ do
+        it "ゲームの文字列を [Frame] に変換する" $ do
+            splitIntoFrames "-- -1 2- 34 -/ 1/ 9/ X X 12" `shouldBe`
+                [ OpenFrame 0 0
+                , OpenFrame 0 1
+                , OpenFrame 2 0
+                , OpenFrame 3 4
+                , SpareFrame 0
+                , SpareFrame 1
+                , SpareFrame 9
+                , StrikeFrame
+                , StrikeFrame
+                , LastFrame 1 2 0
+                ]
+            splitIntoFrames "X X X X X X X X X 3/8" `shouldBe`
+                [ StrikeFrame
+                , StrikeFrame
+                , StrikeFrame
+                , StrikeFrame
+                , StrikeFrame
+                , StrikeFrame
+                , StrikeFrame
+                , StrikeFrame
+                , StrikeFrame
+                , LastFrame 3 10 8
+                ]
+            splitIntoFrames "X X X X X X X X X X-9" `shouldBe`
+                [ StrikeFrame
+                , StrikeFrame
+                , StrikeFrame
+                , StrikeFrame
+                , StrikeFrame
+                , StrikeFrame
+                , StrikeFrame
+                , StrikeFrame
+                , StrikeFrame
+                , LastFrame 10 0 9
+                ]
+            splitIntoFrames "X X X X X X X X X XXX" `shouldBe`
+                [ StrikeFrame
+                , StrikeFrame
+                , StrikeFrame
+                , StrikeFrame
+                , StrikeFrame
+                , StrikeFrame
+                , StrikeFrame
+                , StrikeFrame
+                , StrikeFrame
+                , LastFrame 10 10 10
+                ]
