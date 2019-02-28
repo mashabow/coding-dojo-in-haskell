2fdfeaa3c438701b36baefc09c7d5ec5b757e75c
Masaya Nakamura <mashabow@shiromoji.net>
2019-02-28 22:59:20 +0900

    Introduce optparse-simple to parse command line arguments

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
 module Main where
 
 import           Lib
+import           Options.Applicative.Simple
 
 main :: IO ()
-main = someFunc
+main = do
+    ((), runCmd) <- simpleOptions
+        "version"
+        "coding-dojo-in-haskell CLI"
+        "Run the Kata which you specify as a command"
+        (pure ()) $ do
+        addCommand "someFunc"
+            "print \"someFunc\""
+            (const someFunc)
+            (pure ())
+    runCmd
